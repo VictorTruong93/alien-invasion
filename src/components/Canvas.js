086ae0976e51c292f45b9851b829a44a2006a61c
victor truong <2truong93@gmail.com>
2019-05-13 11:26:11 -0400

    adds Sky component to Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Sky from './Sky';
 
 
 const Canvas = () =>{
@@ -9,6 +10,7 @@ const Canvas = () =>{
         preserveAspectRatio="xMaxYMax none"
         viewBox ={viewBox}
         >
+            <Sky/>
             <circle cx={0} cy={0} r={50} />
         </svg>
     )

652582af66647ac89085e49ec9978a10066ceb63
victor truong <2truong93@gmail.com>
2019-05-14 14:29:12 -0400

    most recent update

--- src/components/CannonBase.js
+++ src/components/CannonBase.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { pathFromBezierCurve } from '../utils/formulas';
 
-const CannonBase = (props) => {
+const CannonBase = () => {
     const cannonBaseStyle ={
         fill: '#a16012',
         stroke: '#75450e',
